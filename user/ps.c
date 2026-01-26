1cf5266742b4100b86a90238f1a53a6b41215c63
aeyzeins <duongkevin62@gmail.com>
2026-01-26 16:58:32 -0500

    Adding state string in ps.c

--- user/ps.c
+++ user/ps.c
@@ -10,4 +10,30 @@ for the STATE column, you must map the internal kernel state integers to the fol
  UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE.
 */
 
+/*
+Defining state cases, numeric values into readable words
+*/
+
+static const char*  //returns pointer to a string literal (read-only)
+
+state_str(int state)
+{
+  switch(state){
+  case 0: return "UNUSED";
+  case 1: return "EMBRYO";
+  case 2: return "SLEEPING";
+  case 3: return "RUNNABLE";
+  case 4: return "RUNNING";
+  case 5: return "ZOMBIE";
+  default: return "UNKNOWN";
+  }
+}
+
+int
+main(void) {
+
+
+    
+}
+
 
