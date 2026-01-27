06169a9ead947b8b957543afdfa663ff21c9708e
aeyzeins <duongkevin62@gmail.com>
2026-01-27 14:32:34 -0500

    declaring variables in proc.h and Makefile

--- kernel/proc.h
+++ kernel/proc.h
@@ -27,6 +27,10 @@ struct cpu {
 };
 
 extern struct cpu cpus[NCPU];
+//Project 1:
+extern struct proc proc[NPROC];
+extern struct spinlock wait_lock;
+
 
 // per-process data for the trap handling code in trampoline.S.
 // sits in a page by itself just under the trampoline page in the
